8b3c4d81232a1a076609303f2d897cc1367b442c
Nic Mendoza <nic.mendoza@gmail.com>
2020-10-16 13:25:15 -0400

    FiscalMonth initial pass at tests (duplicated from FiscalYear and modified where needed)

--- test_fiscalyear.py
+++ test_fiscalyear.py
@@ -647,6 +647,147 @@ class TestFiscalQuarter:
         with pytest.raises(TypeError):
             a >= 1
 
+class TestFiscalMonth:
+
+    @pytest.fixture(scope='class')
+    def a(self):
+        return fiscalyear.FiscalMonth(2016,1)
+
+    @pytest.fixture(scope='class')
+    def b(self):
+        return fiscalyear.FiscalMonth(2016,2)
+
+    @pytest.fixture(scope='class')
+    def c(self):
+        return fiscalyear.FiscalMonth('2016','1')
+
+    @pytest.fixture(scope='class')
+    def d(self):
+        return fiscalyear.FiscalQuarter(2017, 2)
+
+    @pytest.fixture(scope='class')
+    def e(self):
+        return fiscalyear.FiscalMonth(2016, 12)
+
+    def test_basic(self, a):
+        assert a.fiscal_year == 2016
+        assert a.fiscal_month == 1
+
+    def test_current(self, mocker):
+        mock_today = mocker.patch.object(fiscalyear.FiscalDate, 'today')
+        mock_today.return_value = fiscalyear.FiscalDate(2016, 10, 1)
+        current = fiscalyear.FiscalMonth.current()
+        assert current == fiscalyear.FiscalMonth(2017,1)
+
+    def test_repr(self, a):
+        assert repr(a) == 'FiscalMonth(2017,1)'
+
+    def test_str(self, a):
+        assert str(a) == 'FY2017 Period 1'
+
+    def test_from_string(self, c):
+        assert c.fiscal_year == 2017
+
+    def test_wrong_type(self):
+        with pytest.raises(TypeError):
+            fiscalyear.FiscalMonth(2017.5)
+
+        with pytest.raises(TypeError):
+            fiscalyear.FiscalMonth('hello world')
+
+    def test_out_of_range(self):
+        with pytest.raises(ValueError):
+            fiscalyear.FiscalMonth(0)
+
+        with pytest.raises(ValueError):
+            fiscalyear.FiscalMonth(-12)
+
+    def test_prev_fiscal_year(self, a, b):
+        assert a == b.prev_fiscal_month
+
+    def test_next_fiscal_year(self, a, b):
+        assert a.next_fiscal_month == b
+
+    def test_start(self, a):
+        assert a.start == a.year.start
+
+        with fiscalyear.fiscal_calendar(*US_FEDERAL):
+            assert a.start == datetime.datetime(2015, 10, 1, 0, 0, 0)
+
+        with fiscalyear.fiscal_calendar(*UK_PERSONAL):
+            assert a.start == datetime.datetime(2016, 4, 6, 0, 0, 0)
+
+    def test_end(self, a):
+        assert e.end == e.year.end
+
+        with fiscalyear.fiscal_calendar(*US_FEDERAL):
+            assert e.end == datetime.datetime(2016, 9, 30, 23, 59, 59)
+
+        with fiscalyear.fiscal_calendar(*UK_PERSONAL):
+            assert e.end == datetime.datetime(2017, 4, 5, 23, 59, 59)
+
+    # Question: Should FiscalQuarter have m1, m2, m3 properties?
+    # def test_q1(self, a):
+    #     assert a.q1 == fiscalyear.FiscalQuarter(2016, 1)
+
+    # def test_q2(self, a):
+    #     assert a.q2 == fiscalyear.FiscalQuarter(2016, 2)
+
+    # def test_q3(self, a):
+    #     assert a.q3 == fiscalyear.FiscalQuarter(2016, 3)
+
+    # def test_q4(self, a):
+    #     assert a.q4 == fiscalyear.FiscalQuarter(2016, 4)
+
+    def test_contains(self, a, b, c, d):
+        assert b in c
+        assert d not in a
+        assert b in d
+
+        assert fiscalyear.FiscalDateTime(2016, 1, 1, 0, 0, 0) in a
+        assert datetime.datetime(2015, 10, 1, 0, 0, 0) in a
+        assert fiscalyear.FiscalDate(2016, 1, 1) in a
+        assert datetime.date(2015, 10, 1) in a
+
+        with pytest.raises(TypeError):
+            'hello world' in a
+
+    def test_less_than(self, a, b):
+        assert a < b
+
+        with pytest.raises(TypeError):
+            a < 1
+
+    def test_less_than_equals(self, a, b, c):
+        assert a <= b <= c
+
+        with pytest.raises(TypeError):
+            a <= 1
+
+    def test_equals(self, b, c):
+        assert b == c
+
+        with pytest.raises(TypeError):
+            b == 1
+
+    def test_not_equals(self, a, b):
+        assert a != b
+
+        with pytest.raises(TypeError):
+            a != 1
+
+    def test_greater_than(self, a, b):
+        assert b > a
+
+        with pytest.raises(TypeError):
+            a > 1
+
+    def test_greater_than_equals(self, a, b, c):
+        assert c >= b >= a
+
+        with pytest.raises(TypeError):
+            a >= 1
+
 
 class TestFiscalDate:
 
