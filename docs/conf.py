50dbaa5545dbafbe69012dc350b339b5a212e959
Adam J. Stewart <ajstewart426@gmail.com>
2022-02-12 15:40:39 -0600

    Add type hints to docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,7 @@ import fiscalyear  # noqa: E402
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
-needs_sphinx = "1.4"
+needs_sphinx = "4.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -98,64 +97,8 @@ html_theme = "sphinx_rtd_theme"
 # html_static_path = ["_static"]
 
 
-# -- Options for HTMLHelp output ------------------------------------------
+# -- Extension configuration -------------------------------------------------
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = "fiscalyeardoc"
-
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements: Dict[str, str] = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "fiscalyear.tex",
-        "fiscalyear Documentation",
-        fiscalyear.__author__,
-        "manual",
-    )
-]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "fiscalyear", "fiscalyear Documentation", [author], 1)]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "fiscalyear",
-        "fiscalyear Documentation",
-        author,
-        "fiscalyear",
-        "fiscalyear is a small, lightweight Python module providing helpful "
-        "utilities for managing the fiscal calendar.",
-        "Miscellaneous",
-    )
-]
+# sphinx.ext.autodoc
+autodoc_typehints = "description"
+autodoc_typehints_description_target = "documented"
